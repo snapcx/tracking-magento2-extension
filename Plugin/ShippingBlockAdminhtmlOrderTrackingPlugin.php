b77419bd9edc02ab85f079bcd2eacbfea7757c4a
Artem Lubenskyi <aalubenskiy@gmail.com>
2018-03-11 15:33:32 +0200

    SNAPCX-9: refactored Order/Tracking Order/Tracking/View

---
+++ Plugin/ShippingBlockAdminhtmlOrderTrackingPlugin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Jframeworks\Shippingtracking\Plugin;
+
+class ShippingBlockAdminhtmlOrderTrackingPlugin
+{
+    protected $_scopeConfig;
+    protected $_carrierFactory;
+
+    public function __construct(
+        \Magento\Shipping\Model\CarrierFactory $carrierFactory,
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        $this->_carrierFactory = $carrierFactory;
+        $this->_scopeConfig = $context->getScopeConfig();
+    }
+
+    public function afterGetCarriers(\Magento\Shipping\Block\Adminhtml\Order\Tracking $subject, $result)
+    {
+        if ($this->getIsActive()) {
+            $api_url ="https://api.snapcx.io/tracking/v1/getCarriers";
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $api_url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            // Get response
+            $response = curl_exec($curl);
+            // Get HTTP status code
+            $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            // Close cURL
+            curl_close($curl);
+
+            $response = json_decode($response);
+
+            $result = [];
+            $result['custom'] = __('Custom Value');
+            foreach ($response as $key => $value) {
+                $result[$value->carrierCode] = $value->carrierName;
+
+                /* $result[] = array('value' => $value->carrierCode , 'label' => $value->carrierName); */
+            }
+        }
+
+        return $result;
+    }
+    public function getIsActive()
+    {
+        return ($this->_scopeConfig->getValue('shippingtracking/shippingtracking_settings/enable', \Magento\Store\Model\ScopeInterface::SCOPE_STORE) == "1" );
+    }
+}
