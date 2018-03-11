040bace96117c82272c23929619f94532e6d7502
Artem Lubenskyi <aalubenskiy@gmail.com>
2018-03-11 22:44:41 +0200

    SNAPCX-9: added 'if extension is enabled' condition

--- Plugin/ShippingModelOrderTrackPlugin.php
+++ Plugin/ShippingModelOrderTrackPlugin.php
@@ -4,12 +4,32 @@ namespace Jframeworks\Shippingtracking\Plugin;
 
 class ShippingModelOrderTrackPlugin
 {
+    protected $_scopeConfig;
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        $this->_scopeConfig = $context->getScopeConfig();
+    }
+
     public function afterGetNumberDetail(\Magento\Shipping\Model\Order\Track $subject, $result)
     {
-        return array(
-            'tracking_number' => $subject->getTrackNumber(),
-            'title' => $subject->getTitle(),
-            'carrier_code' => $subject->getCarrierCode()
+        if ($this->getIsActive()) {
+            $result = array(
+                'tracking_number' => $subject->getTrackNumber(),
+                'title' => $subject->getTitle(),
+                'carrier_code' => $subject->getCarrierCode()
+            );
+        }
+
+        return $result;
+    }
+
+    public function getIsActive()
+    {
+        return (
+            $this->_scopeConfig->getValue('shippingtracking/shippingtracking_settings/enable', \Magento\Store\Model\ScopeInterface::SCOPE_STORE) == "1"
         );
     }
 }
